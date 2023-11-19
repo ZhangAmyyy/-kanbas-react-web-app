358d1d4894027a26c41b5ffe1de4830f1edb6c13
ZhangAmyyy <zhangyy_amy@outlook.com>
2023-11-19 15:25:34 -0800

    change url in lab

--- src/Labs/a5/WorkingWithArrays.js
+++ src/Labs/a5/WorkingWithArrays.js
@@ -10,7 +10,7 @@ function WorkingWithArrays() {
         completed: false,
       });
     
-    const API = "http://localhost:4000/a5/todos";
+    const API = "https://kanbas-node-server-app-qyrs.onrender.com/a5/todos";
     // const [todo, setTodo] = useState({});
     const [todos, setTodos] = useState([]);
     const postTodo = async () => {
