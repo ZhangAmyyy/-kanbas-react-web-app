5dff52e454935c6325c889e3fab55d1e148716c4
ZhangAmyyy <zhangyy_amy@outlook.com>
2023-12-03 05:31:28 -0800

    a6

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Kanbas from "./Kanbas";
 import { HashRouter } from "react-router-dom";
 import { Routes, Route, Navigate } from "react-router";
 
+import Project from "./project";
+
 
 function App() {
    return (
@@ -12,6 +14,7 @@ function App() {
          <div>
             <Routes>
             <Route path="/" element={<Navigate to="/Labs"/>}/>
+            <Route path="/project/*" element={<Project />} />
             <Route path="/hello" element={<HelloWorld />} />
             <Route path="/Labs/*" element={<Labs />} />
             <Route path="/Kanbas/*" element={<Kanbas />} />
